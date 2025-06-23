ee00d69f202ebfc6241cb88cb5d85b4c2bb0fb0a
ssb <ssb@sibdigital.net>
2025-06-23 08:04:37 +0800

    feature/RXJS-3: add switchMap, forkJoin, combineLatest and iif

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ICart, IProduct } from '../utils/types';
 
 @Injectable({ providedIn: 'root' })
 export class BaseService {
   private apiUrl = 'https://fakestoreapi.com/products';
+  private cartsUrl = 'https://fakestoreapi.com/carts';
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<unknown[]> {
-    return this.http.get<unknown[]>(this.apiUrl);
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.apiUrl);
+  }
+
+  getCarts(): Observable<ICart[]> {
+    return this.http.get<ICart[]>(this.cartsUrl);
   }
 }
--- src/app/store/base.store.ts
+++ src/app/store/base.store.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { BaseService } from '../services/base.service';
+import { ICart, IProduct } from '../utils/types';
 
 @Injectable({ providedIn: 'root' })
 export class BaseStore {
-  private productsSubject = new BehaviorSubject<unknown[]>([]);
+  private productsSubject = new BehaviorSubject<IProduct[]>([]);
   products$ = this.productsSubject.asObservable();
+  private cartsSubject = new BehaviorSubject<ICart[]>([]);
+  carts$ = this.cartsSubject.asObservable();
 
   constructor(private service: BaseService) {
     this.loadProducts();
+    this.loadCarts();
   }
 
   loadProducts(): void {
     this.service.getProducts().subscribe((products) => {
       this.productsSubject.next(products);
+    });
+  }
+
+  loadCarts(): void {
+    this.service.getCarts().subscribe((carts) => {
+      this.cartsSubject.next(carts);
     });
   }
 }
